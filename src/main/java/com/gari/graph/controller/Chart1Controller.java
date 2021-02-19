fd6e65dac825718333d71b0f533fc17235cbc1b3
Garichek <tsegarik@gmail.com>
2021-02-19 12:13:23 +0200

    add additional data line to Altitude Tab

--- src/main/java/com/gari/graph/controller/Chart1Controller.java
+++ src/main/java/com/gari/graph/controller/Chart1Controller.java
@@ -141,7 +141,8 @@ public class Chart1Controller implements Initializable {
         String jsonPointsData = inputPointsArea.getText();
 
         // Update Charts
-        updateChartData(inputSynchroSeconds, altChart, jsonPointsData, TemporaryPoint::getAltitude, 1d);
+        updateChartDataTheeLines(inputSynchroSeconds, altChart, jsonPointsData, TemporaryPoint::getAltitude, TemporaryPoint::getAltitude, TemporaryPoint::getReliefHeight, 1d);
+
         updateChartData(inputSynchroSeconds, latChart, jsonPointsData, TemporaryPoint::getLatitude, 0.000001d);
         updateChartData(inputSynchroSeconds, longChart, jsonPointsData, TemporaryPoint::getLongitude, 0.000001d);
         updateChartData(inputSynchroSeconds, vSpeedChart, jsonPointsData, TemporaryPoint::getVerticalSpeed, 1d);
@@ -155,14 +156,12 @@ public class Chart1Controller implements Initializable {
 
     // Parse Data for Charts
     private void updateChartData(int synchroSeconds, LineChart<String, Number> chart, String jsonDataPoints, Function<TemporaryPoint, Number> parameter, Double tickValue) {
-
         if (StringUtils.isBlank(jsonDataPoints)) {
             log.error("Json Data is EMPTY");
             return;
         }
 
         PointsData pointsData = getPointsDataFromJson(jsonDataPoints);
-
         if (Objects.isNull(pointsData)) {
             log.error("PointsData not converted");
             return;
@@ -183,7 +182,6 @@ public class Chart1Controller implements Initializable {
             simPoints.add(0, firstSimPoint);
         }
 
-
         AtomicInteger countSim = new AtomicInteger(1);
         List<XYChart.Data<String, Number>> simData = simPoints.stream().map(point -> new XYChart.Data<>(String.format("%s", countSim.getAndIncrement()), parameter.apply(point))).collect(Collectors.toList());
 
@@ -196,7 +194,6 @@ public class Chart1Controller implements Initializable {
 
     // Create CHART View
     private void updateChart(LineChart<String, Number> chart, List<XYChart.Data<String, Number>> simData, List<XYChart.Data<String, Number>> realData, double tickValue) {
-
         double minValueSim = simData.stream().mapToDouble(point -> (double) point.getYValue()).min().orElse(0) - tickValue * 10.;
         double maxValueSim = simData.stream().mapToDouble(point -> (double) point.getYValue()).max().orElse(0) + tickValue * 10.;
         double minValueReal = realData.stream().mapToDouble(point -> (double) point.getYValue()).min().orElse(0) - tickValue * 10.;
@@ -221,7 +218,6 @@ public class Chart1Controller implements Initializable {
 
         chart.getData().add(seriesReal);
 
-
         Platform.runLater(()
                 -> {
 
@@ -244,9 +240,116 @@ public class Chart1Controller implements Initializable {
             seriesReal.getNode().lookup(".chart-series-line").setStyle("-fx-stroke: blue; -fx-background-color: blue, white;");
             seriesSim.getNode().lookup(".chart-series-line").setStyle("-fx-stroke: red;");
         });
-
-
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Parse Data for Charts
+    private void updateChartDataTheeLines(int synchroSeconds, LineChart<String, Number> chart, String jsonDataPoints, Function<TemporaryPoint, Number> parameter1, Function<TemporaryPoint, Number> parameter2, Function<TemporaryPoint, Number> parameter3, Double tickValue) {
+        if (StringUtils.isBlank(jsonDataPoints)) {
+            log.error("Json Data is EMPTY");
+            return;
+        }
+
+        PointsData pointsData = getPointsDataFromJson(jsonDataPoints);
+        if (Objects.isNull(pointsData)) {
+            log.error("PointsData not converted");
+            return;
+        }
+
+        List<TemporaryPoint> simPoints = pointsData.getSimPoints();
+        List<TemporaryPoint> realPoints = pointsData.getRealPoints();
+
+        if (CollectionUtils.isEmpty(simPoints) || CollectionUtils.isEmpty(realPoints)) {
+            log.error("Some of Points Data is Empty [SIM or REAL]");
+            return;
+        }
+
+        // Little synchronize
+        TemporaryPoint firstSimPoint = simPoints.get(0);
+
+        for (int i = 0; i <= synchroSeconds; i++) {
+            simPoints.add(0, firstSimPoint);
+        }
+
+        AtomicInteger countSim = new AtomicInteger(1);
+        List<XYChart.Data<String, Number>> simData = simPoints.stream().map(point -> new XYChart.Data<>(String.format("%s", countSim.getAndIncrement()), parameter1.apply(point))).collect(Collectors.toList());
+
+        AtomicInteger countReal = new AtomicInteger(1);
+        List<XYChart.Data<String, Number>> realData = realPoints.stream().map(point -> new XYChart.Data<>(String.format("%s", countReal.getAndIncrement()), parameter2.apply(point))).collect(Collectors.toList());
+
+        AtomicInteger additionalCount = new AtomicInteger(1);
+        List<XYChart.Data<String, Number>> additionalData = realPoints.stream().map(point -> new XYChart.Data<>(String.format("%s", additionalCount.getAndIncrement()), parameter3.apply(point))).collect(Collectors.toList());
+
+
+        updateChartThreeLines(chart, simData, realData, additionalData, tickValue);
+    }
+
+
+    // Create CHART View
+    private void updateChartThreeLines(LineChart<String, Number> chart, List<XYChart.Data<String, Number>> simData, List<XYChart.Data<String, Number>> realData, List<XYChart.Data<String, Number>> additionalData, double tickValue) {
+        double minValueSim = simData.stream().mapToDouble(point -> (double) point.getYValue()).min().orElse(0) - tickValue * 10.;
+        double maxValueSim = simData.stream().mapToDouble(point -> (double) point.getYValue()).max().orElse(0) + tickValue * 10.;
+        double minValueReal = realData.stream().mapToDouble(point -> (double) point.getYValue()).min().orElse(0) - tickValue * 10.;
+        double maxValueReal = realData.stream().mapToDouble(point -> (double) point.getYValue()).max().orElse(0) + tickValue * 10.;
+
+        NumberAxis yNumberAx = (NumberAxis) chart.getYAxis();
+        yNumberAx.setAutoRanging(false);
+        yNumberAx.setLowerBound(Math.min(minValueSim, minValueReal));
+        yNumberAx.setUpperBound(Math.max(maxValueSim, maxValueReal));
+        yNumberAx.setTickUnit(tickValue);
+
+
+        XYChart.Series<String, Number> seriesSim = new XYChart.Series<>();
+        seriesSim.setName("SIM");
+        seriesSim.getData().addAll(simData);
+        chart.getData().add(seriesSim);
+
+        XYChart.Series<String, Number> seriesReal = new XYChart.Series<>();
+        seriesReal.setName("REAL");
+        seriesReal.getData().addAll(realData);
+        chart.getData().add(seriesReal);
+
+        XYChart.Series<String, Number> seriesAdditional = new XYChart.Series<>();
+        seriesAdditional.setName("Additional");
+        seriesAdditional.getData().addAll(additionalData);
+        chart.getData().add(seriesAdditional);
+
+
+        Platform.runLater(()
+                -> {
+            Set<Node> nodes2 = chart.lookupAll(".series" + 2);
+            for (Node n : nodes2) {
+                n.setStyle("-fx-background-color: black, black;\n"
+                        + "    -fx-background-insets: 0, 2;\n"
+                        + "    -fx-background-radius: 1px;\n"
+                        + "    -fx-padding: 1px;");
+            }
+
+            Set<Node> nodes1 = chart.lookupAll(".series" + 1);
+            for (Node n : nodes1) {
+                n.setStyle("-fx-background-color: blue, white;\n"
+                        + "    -fx-background-insets: 0, 2;\n"
+                        + "    -fx-background-radius: 5px;\n"
+                        + "    -fx-padding: 5px;");
+            }
+
+            Set<Node> nodes = chart.lookupAll(".series" + 0);
+            for (Node n : nodes) {
+                n.setStyle("-fx-background-color: red, white;\n"
+                        + "    -fx-background-insets: 0, 2;\n"
+                        + "    -fx-background-radius: 5px;\n"
+                        + "    -fx-padding: 5px;");
+            }
+
+            seriesReal.getNode().lookup(".chart-series-line").setStyle("-fx-stroke: blue;");
+            seriesSim.getNode().lookup(".chart-series-line").setStyle("-fx-stroke: red;");
+            seriesAdditional.getNode().lookup(".chart-series-line").setStyle("-fx-stroke: black;");
+        });
+    }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     private PointsData getPointsDataFromJson(String jsonDataPoints) {
